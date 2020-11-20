c0eb1f9e89c22ddc2172a3732ff31e6a87cc6451
Adrian Curet <curetmicael@gmail.com>
2020-11-20 18:49:29 -0400

    Base de Datos Realizada

---
+++ Tarea9y10/Migrations/20201120183310_InitialData.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tarea9y10.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DatosAcademicos",
+                columns: table => new
+                {
+                    DatosAcademicosId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NivelEstudio = table.Column<string>(type: "TEXT", nullable: true),
+                    ExplusadoSuspendido = table.Column<string>(type: "TEXT", nullable: true),
+                    RazonEXSUS = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatosAcademicos", x => x.DatosAcademicosId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DatosEclesiasticos",
+                columns: table => new
+                {
+                    DatosEclesiasticosId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FechaConversion = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FechaBautismo = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FechaAceptadoIglesia = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DenominacionPerteneciente = table.Column<string>(type: "TEXT", nullable: true),
+                    NombreIglesiaActual = table.Column<string>(type: "TEXT", nullable: true),
+                    IglesiaAnterior = table.Column<string>(type: "TEXT", nullable: true),
+                    NombrePastorActual = table.Column<string>(type: "TEXT", nullable: true),
+                    Disciplinatura = table.Column<string>(type: "TEXT", nullable: true),
+                    CausaDisciplina = table.Column<string>(type: "TEXT", nullable: true),
+                    FuncionesOcupa = table.Column<string>(type: "TEXT", nullable: true),
+                    MinisterioServidos = table.Column<string>(type: "TEXT", nullable: true),
+                    MinisteriosFrutosPQ = table.Column<string>(type: "TEXT", nullable: true),
+                    DiosLlamado = table.Column<string>(type: "TEXT", nullable: true),
+                    MetasVida = table.Column<string>(type: "TEXT", nullable: true),
+                    RespaldoEstudios = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatosEclesiasticos", x => x.DatosEclesiasticosId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DatosFamiliares",
+                columns: table => new
+                {
+                    DatosFamiliaresId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EstadoCivil = table.Column<string>(type: "TEXT", nullable: true),
+                    NombrePareja = table.Column<string>(type: "TEXT", nullable: true),
+                    CantidadHijos = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatosFamiliares", x => x.DatosFamiliaresId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DatosLaborales",
+                columns: table => new
+                {
+                    DatosLaboralesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Profesion = table.Column<string>(type: "TEXT", nullable: true),
+                    OcupacionActual = table.Column<string>(type: "TEXT", nullable: true),
+                    NomEmpresaNegocio = table.Column<string>(type: "TEXT", nullable: true),
+                    TelefonoEmpresaNegocio = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatosLaborales", x => x.DatosLaboralesId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Direccion",
+                columns: table => new
+                {
+                    DireccionId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Provincia = table.Column<string>(type: "TEXT", nullable: true),
+                    Sector = table.Column<string>(type: "TEXT", nullable: true),
+                    Calle = table.Column<string>(type: "TEXT", nullable: true),
+                    NumVivienda = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Direccion", x => x.DireccionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocumentoIdentificacion",
+                columns: table => new
+                {
+                    DocIdentidadId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Cedula = table.Column<string>(type: "TEXT", nullable: true),
+                    RNC = table.Column<string>(type: "TEXT", nullable: true),
+                    Pasaporte = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentoIdentificacion", x => x.DocIdentidadId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Integrantes",
+                columns: table => new
+                {
+                    IntegranteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(type: "TEXT", nullable: true),
+                    Apellidos = table.Column<string>(type: "TEXT", nullable: true),
+                    Sexo = table.Column<string>(type: "TEXT", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PaisNacimiento = table.Column<string>(type: "TEXT", nullable: true),
+                    Foto = table.Column<string>(type: "TEXT", nullable: true),
+                    CiudadNacimiento = table.Column<string>(type: "TEXT", nullable: true),
+                    PaisResidenciaAct = table.Column<string>(type: "TEXT", nullable: true),
+                    CiudadResidenciaAct = table.Column<string>(type: "TEXT", nullable: true),
+                    DireccionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: true),
+                    Celular = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    DocIdentidadId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DatosFamiliaresId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DatosLaboralesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DatosEclesiasticosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DatosAcademicosId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Integrantes", x => x.IntegranteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DatosAcademicos");
+
+            migrationBuilder.DropTable(
+                name: "DatosEclesiasticos");
+
+            migrationBuilder.DropTable(
+                name: "DatosFamiliares");
+
+            migrationBuilder.DropTable(
+                name: "DatosLaborales");
+
+            migrationBuilder.DropTable(
+                name: "Direccion");
+
+            migrationBuilder.DropTable(
+                name: "DocumentoIdentificacion");
+
+            migrationBuilder.DropTable(
+                name: "Integrantes");
+        }
+    }
+}
---
+++ Tarea9y10/Models/AplicacionDbContext.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tarea9y10.Models
+{
+    public class AplicacionDbContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite("Data Source=IglesiaData.db");
+
+        public DbSet<Integrantes> Integrantes { get; set; }
+        public DbSet<Direccion> Direccion { get; set; }
+
+        public DbSet<DocumentoIdentificacion> DocumentoIdentificacion { get; set; }
+        public DbSet<DatosFamiliares> DatosFamiliares { get; set; }
+        public DbSet<DatosLaborales> DatosLaborales { get; set; }
+        public DbSet<DatosEclesiasticos> DatosEclesiasticos { get; set; }
+        public DbSet<DatosAcademicos> DatosAcademicos { get; set; }
+    }
+
+    public class Integrantes
+    {
+        [Key]
+        public int IntegranteId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string PaisNacimiento { get; set; }
+        public string Foto { get; set; }
+        public string CiudadNacimiento { get; set; }
+        public string PaisResidenciaAct { get; set; }
+        public string CiudadResidenciaAct { get; set; }
+        public int DireccionId { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+        public int DocIdentidadId { get; set; }
+        public int DatosFamiliaresId { get; set; }
+        public int DatosLaboralesId { get; set; }
+        public int DatosEclesiasticosId { get; set; }
+        public int DatosAcademicosId { get; set; }
+
+
+    }
+
+    public class Direccion
+    {
+        [Key]
+        public int DireccionId { get; set; }
+        public string Provincia { get; set; }
+        public string Sector { get; set; }
+        public string Calle { get; set; }
+        public string NumVivienda { get; set; }
+    }
+
+    public class DocumentoIdentificacion
+    {
+        [Key]
+        public int DocIdentidadId { get; set; }
+        public string Cedula { get; set; }
+        public string RNC { get; set; }
+        public string Pasaporte { get; set; }
+    }
+
+    public class DatosFamiliares
+    {
+        [Key]
+        public int DatosFamiliaresId { get; set; }
+        public string EstadoCivil { get; set; }
+        public string NombrePareja { get; set; }
+
+        public string CantidadHijos { get; set; }
+    }
+
+    public class DatosLaborales
+    {
+        [Key]
+        public int DatosLaboralesId { get; set; }
+        public string Profesion { get; set; }
+        public string OcupacionActual { get; set; }
+        public string NomEmpresaNegocio { get; set; }
+        public string TelefonoEmpresaNegocio { get; set; }
+    }
+
+    public class DatosEclesiasticos
+    {
+        [Key]
+        public int DatosEclesiasticosId { get; set; }
+
+        public DateTime FechaConversion { get; set; }
+        public DateTime FechaBautismo { get; set; }
+        public DateTime FechaAceptadoIglesia { get; set; }
+
+        public string DenominacionPerteneciente { get; set; }
+
+        public string NombreIglesiaActual { get; set; }
+
+        public string IglesiaAnterior { get; set; }
+
+        public string NombrePastorActual { get; set; }
+
+        public string Disciplinatura { get; set; }
+
+        public string CausaDisciplina { get; set; }
+
+        public string FuncionesOcupa { get; set; }
+
+        public string MinisterioServidos { get; set; }
+        public string MinisteriosFrutosPQ { get; set; }
+
+        public string DiosLlamado { get; set; }
+        public string MetasVida { get; set; }
+
+        public string RespaldoEstudios {get;set;}
+    }
+
+    public class DatosAcademicos
+    {
+        [Key]
+        public int DatosAcademicosId { get; set; }
+
+        public string NivelEstudio { get; set; }
+        public string ExplusadoSuspendido { get; set; }
+
+        public string RazonEXSUS { get; set; }
+    }
+}
