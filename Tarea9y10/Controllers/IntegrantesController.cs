f3a0c5a408ec1f1b8d1252aa137c60fc3eb232d2
Adrian Curet <curetmicael@gmail.com>
2020-11-22 19:57:45 -0400

    Parte Create Creada

--- Tarea9y10/Controllers/IntegrantesController.cs
+++ Tarea9y10/Controllers/IntegrantesController.cs
@@ -15,23 +15,78 @@ namespace Tarea9y10.Controllers
         {
             return View();
         }
+
         [HttpGet]
         [Route("create")]
         public IActionResult Create()
         {
 
-            return View("Create", new Integrantes());
+            return View("Create", new ViewModel());
 
         }
         [HttpPost]
         [Route("create")]
-        public IActionResult Create(Integrantes integrantes)
+        public IActionResult Create(Integrantes integrantes,Direccion direccion,DocumentoIdentificacion documento,DatosFamiliares familiares,DatosAcademicos academicos,DatosEclesiasticos eclesiasticos,DatosLaborales laborales)
         {
+            string DocIdent = Request.Form["identificacion"];
 
-                bd.Integrantes.Add(integrantes);
-                bd.SaveChanges();
-                return RedirectToAction("Index");
+            if (DocIdent == "Cedula")
+            {
+                //documento.Cedula
+            }
+            else if (DocIdent == "RNC")
+            {
+                //documento.RNC
+            }
+            else
+            {
+                //documento.Pasaporte
+            }
+
+            bd.DocumentoIdentificacion.Add(documento);
+            bd.SaveChanges();
+            bd.DatosFamiliares.Add(familiares);
+            bd.SaveChanges();
+            bd.DatosAcademicos.Add(academicos);
+            bd.SaveChanges();
+            bd.DatosEclesiasticos.Add(eclesiasticos);
+            bd.SaveChanges();
+            bd.DatosLaborales.Add(laborales);
+            bd.SaveChanges();
+            bd.Direccion.Add(direccion);
+            bd.SaveChanges();
+
+            foreach (var direc in bd.Direccion)
+            {
+                integrantes.DireccionId = direc.DireccionId;
+            }
+            foreach (var fam in bd.DatosFamiliares)
+            {
+                integrantes.DatosFamiliaresId = fam.DatosFamiliaresId;
+            }
+            foreach (var aca in bd.DatosAcademicos)
+            {
+                integrantes.DatosAcademicosId = aca.DatosAcademicosId;
+            }
+            foreach (var ecle in bd.DatosEclesiasticos)
+            {
+                integrantes.DatosEclesiasticosId = ecle.DatosEclesiasticosId;
+            }
+            foreach (var doc in bd.DocumentoIdentificacion)
+            {
+                integrantes.DocIdentidadId = doc.DocIdentidadId;
+            }
+            foreach (var lab in bd.DatosLaborales)
+            {
+                integrantes.DatosLaboralesId = lab.DatosLaboralesId;
+            }
+
+            bd.Integrantes.Add(integrantes);
+            bd.SaveChanges();
+
+            return RedirectToAction("Index");
            
         }
+
     }
 }
---
+++ Tarea9y10/Models/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarea9y10.Models
+{
+    public class ViewModel
+    {
+        public Integrantes integrantes { get; set; }
+        public Direccion direccion { get; set; }
+        public DocumentoIdentificacion documentoIdentificacion { get; set; }
+        public DatosAcademicos academicos { get; set; }
+        public DatosEclesiasticos eclesiasticos { get; set; }
+        public DatosLaborales laborales { get; set; }
+        public DatosFamiliares familiares { get; set; }
+    }
+}
